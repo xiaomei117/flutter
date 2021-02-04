e6784ccef2c7f64192bb905f393c94bb6e4db81f
xiaomei <963717473@qq.com>
2021-02-04 20:44:11 +0800

    添加手势监听事件，点击recent view头像也可跳转页面，并修改神婆名字

--- flutter_app/lib/widget/recent_star.dart
+++ flutter_app/lib/widget/recent_star.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/models/star.dart';
 
 import 'package:flutter_app/widget/share_preference_url.dart';
+import 'package:flutter_app/widget/stars_info.dart';
 import 'get_sticky_tab.dart';
 import 'list_starinfo.dart';
 import 'package:sticky_headers/sticky_headers.dart';
@@ -13,6 +14,8 @@ class RowStars extends StatefulWidget {
 }
 
 class _RowStarsState extends State<RowStars> {
+  SharePreference starID = SharePreference.starPer;
+
   @override
   Widget build(BuildContext context) {
     return starRecentViewed();
@@ -41,32 +44,42 @@ class _RowStarsState extends State<RowStars> {
               itemCount: len,
               itemExtent: 86,
               itemBuilder: (context, index) {
-                return Column(
-                  children: [
-                    Container(
-                      height: 70,
-                      width: 70,
-                      decoration: ShapeDecoration(
-                        image: new DecorationImage(
-                            //设置背景图片
-                            image: NetworkImage(searchStar(
-                                    SharePreference.listID[len - 1 - index])
-                                .avatar),
-                            fit: BoxFit.cover),
-                        //设置圆角
-                        shape: RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadiusDirectional.circular(100)),
-                      ),
-                    ),
-                    SizedBox(height: 4),
-                    Container(
+                return GestureDetector(
+                  onTap: () {
+                    starID.writeList('RecentViewed',searchStar(SharePreference.listID[len - 1 - index]).userId);
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => new StarsInfo(star: searchStar(SharePreference.listID[index]))),
+                    );
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 70,
                         width: 70,
-                        child: Text(
-                            searchStar(SharePreference.listID[len - 1 - index])
-                                .name,
-                            overflow: TextOverflow.ellipsis))
-                  ],
+                        decoration: ShapeDecoration(
+                          image: new DecorationImage(
+                              //设置背景图片
+                              image: NetworkImage(searchStar(
+                                      SharePreference.listID[len - 1 - index])
+                                  .avatar),
+                              fit: BoxFit.cover),
+                          //设置圆角
+                          shape: RoundedRectangleBorder(
+                              borderRadius:
+                                  BorderRadiusDirectional.circular(100)),
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                      Container(
+                          width: 70,
+                          child: Text(
+                              searchStar(SharePreference.listID[len - 1 - index])
+                                  .name,
+                              overflow: TextOverflow.ellipsis))
+                    ],
+                  ),
                 );
               }),
         ),
